2bee30e68410d6d633addf74149f33320326a09b
joniewebdev <joniebechayda27@gmail.com>
2025-09-30 20:22:28 +0800

    Changes to be committed: new file:   home.html modified:   index.html modified:   script.js

--- script.js
+++ script.js
@@ -11,52 +11,23 @@ const supa = createClient(
 
 document.getElementById("login-form").addEventListener("submit", function(event) {
   event.preventDefault();
-  const user = document.getElementById("username").value;
+  const user = document.getElementById("email").value;
   const pass = document.getElementById("password").value;
   
   loginUser(user, pass);
 });
 
-async function loginUser(username, password) {
-    var { data, error } = await supa
-        .from("user_login")
-        .select("*")
-        .eq("user_login_id", username)
-        .eq("password", password);
+async function loginUser(email, password) {
+    var { data, error } = await supa.auth.signInWithPassword({
+        email: email,
+        password: password
+    });
     if (error) {
         document.getElementById("login-message").innerText = "Login failed: " + error.message;
         return;
     }
-    if(data.length === 0) {
-        document.getElementById("login-message").innerText = "Invalid username or password.";
-        return;
-    }
-    let person_id = data[0].person_id;
-    var { data, error } = await supa
-        .from("person")
-        .select("*")
-        .eq("person_id", person_id)
-        .single();
-    document.getElementById("login-message").innerText = "Login successful. Welcome, " + data.first_name + "!";
-}
-async function getPersons() {
-    let { data, error } = await supa.from("person").select("*");
-    if (error) {
-        document.getElementById("persons").innerText = "Error: " + error.message;
-        return;
-    }
-    if (data.length === 0) {
-        document.getElementById("persons").innerText = "No students found.";
-        return;
-    }
-    
-    data.forEach(person => {
-        document.getElementById("persons").innerHTML += `
-            <div class="card">
-                <h2>${person.first_name} ${person.middle_name[0]}. ${person.last_name} ${person.suffix !== null ? person.suffix : ''}</h2>
-                <p>Address: ${person.address}</p>
-                <p>Birthday: ${person.birth_date}</p>
-            </div>
-        `;
-    });
+    document.getElementById("login-message").innerText = "Login successful!";
+    setTimeout(() => {
+      window.location.href = "./home.html";
+    }, 2000);
 }
