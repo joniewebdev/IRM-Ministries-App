89e9ca353c85b4bc0dc6bdd3222eebe2d4262145
joniewebdev <joniebechayda27@gmail.com>
2025-09-30 20:56:12 +0800

    new file:   auth.js 	modified:   home.html 	modified:   script.js

--- script.js
+++ script.js
@@ -9,17 +9,25 @@ const supa = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhdW5hamJ0YnhvZXZqdW56ZXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNTE5MTIsImV4cCI6MjA3NDcyNzkxMn0.0kHj3gpuQuQ5iJR-HFWT4uHlj6BQ9B_JCFHMVgbTlvA"
 );
 
+import { requireAuth, logout } from "./auth.js";
+
+// Protect this page
+const session = await requireAuth();
+
+if (!session) {
+    window.location.href = "./index.html";
+}
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    logout();
+});
+
 document.getElementById("login-form").addEventListener("submit", function(event) {
   event.preventDefault();
   const user = document.getElementById("email").value;
   const pass = document.getElementById("password").value;
   
   loginUser(user, pass);
-});
-
-document.getElementById("logoutBtn").addEventListener("click", async () => {
-    await supa.auth.signOut();
-    window.location.href = "./index.html";
 });
 
 async function loginUser(email, password) {
@@ -36,15 +44,3 @@ async function loginUser(email, password) {
       window.location.href = "./home.html";
     }, 2000);
 }
-
-async function checkAuth() {
-    const { data: { session } } = await supa.auth.getSession();
-
-    if (!session) {
-    window.location.href = "./index.html";
-    } else {
-        window.location.href = "./home.html";
-    }
-}
-
-checkAuth();
