c252d3841f94222901859e43c7610d36dbcd28f7
joniewebdev <joniebechayda27@gmail.com>
2025-09-30 21:44:53 +0800

    modified:   js/supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://vaunajbtbxoevjunzerj.supabase.co",
-  "your-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9-key"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhdW5hamJ0YnhvZXZqdW56ZXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNTE5MTIsImV4cCI6MjA3NDcyNzkxMn0.0kHj3gpuQuQ5iJR-HFWT4uHlj6BQ9B_JCFHMVgbTlvA"
 );
