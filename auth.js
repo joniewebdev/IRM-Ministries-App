987037043b9cf745a98d8a28068410546d996d3b
joniewebdev <joniebechayda27@gmail.com>
2025-09-30 21:20:15 +0800

    modified:   auth.js

--- auth.js
+++ auth.js
@@ -6,11 +6,6 @@ export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function requireAuth(redirectTo = "index.html") {
   const { data: { session } } = await supabase.auth.getSession();
-
-  if (!session) {
-    window.location.href = redirectTo;
-  }
-
   return session;
 }
 
