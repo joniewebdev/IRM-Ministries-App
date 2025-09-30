c5ec321af43be99394ae3a772b11d1cca3de4065
joniewebdev <joniebechayda27@gmail.com>
2025-09-30 21:00:54 +0800

    modified:   script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const supa = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhdW5hamJ0YnhvZXZqdW56ZXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNTE5MTIsImV4cCI6MjA3NDcyNzkxMn0.0kHj3gpuQuQ5iJR-HFWT4uHlj6BQ9B_JCFHMVgbTlvA"
 );
 
-import { requireAuth, logout } from "./auth.js";
+import { requireAuth, logout } from "auth.js";
 
 // Protect this page
 const session = await requireAuth();
