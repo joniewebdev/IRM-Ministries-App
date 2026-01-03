5a8a837338b688753e7b1bc36a0dd865799a7521
joniewebdev <joniebechayda27@gmail.com>
2026-01-03 11:07:08 +0800

    modified:   js/sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("app-cache-v1").then((cache) => {
+    caches.open("app-cache-v2").then((cache) => {
       return cache.addAll([
         "/",               // root
         "./index.html",
